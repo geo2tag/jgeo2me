2f9064669c1baf37ded986987db7fd57a4d47040
Mark Zaslavskiy <mark.zaslavskiy>
2013-08-01 12:20:21 +0400

    added new interfaces for date and location operations.

--- jgeo2me/src/ru/spb/osll/utils/DateUtil.java
+++ jgeo2me/src/ru/spb/osll/utils/DateUtil.java
@@ -40,9 +40,7 @@ public class DateUtil {
         
         public static String getTime(Date date){
                 Calendar cal = Calendar.getInstance();
-                cal.setTime(date);
-                
-                
+                cal.setTime(date);          
                 
                 int day = cal.get(Calendar.DAY_OF_MONTH);
                 int month = cal.get(Calendar.MONTH)+1;
@@ -53,7 +51,7 @@ public class DateUtil {
                 int millisecond = cal.get(Calendar.MILLISECOND);
                 
                 System.out.println("Month " + month);
-//      "dd MM yyyy HH:mm:ss.SSS";              
+                // Format = "dd MM yyyy HH:mm:ss.SSS";              
                 
                 String currentTime = 
                                 identNumberWithZeros(day,2) + " " +
@@ -80,4 +78,23 @@ public class DateUtil {
                 return getTime(cal.getTime());
         }
 	
+        public static Date fromString(String dateString){
+        	Date date = null;
+        	
+            Calendar cal = Calendar.getInstance();
+            int day = Integer.parseInt(dateString.substring(0, 1));
+            int month = Integer.parseInt(dateString.substring(3, 4)) - 1;
+            int year = Integer.parseInt(dateString.substring(6, 9));
+            int hour = Integer.parseInt(dateString.substring(11, 12));
+            int minute = Integer.parseInt(dateString.substring(14, 15));
+            int second = Integer.parseInt(dateString.substring(17, 18));
+            int millisecond = Integer.parseInt(dateString.substring(20, 22));
+        	
+            cal.set(year, month, day, hour, minute, second);
+            cal.set(Calendar.MILLISECOND, millisecond);
+            
+            date = cal.getTime();
+            
+        	return date;
+        }
 }
--- jgeo2me/src/ru/spb/osll/utils/LocationUtil.java
+++ jgeo2me/src/ru/spb/osll/utils/LocationUtil.java
@@ -9,7 +9,7 @@ import javax.microedition.location.LocationProvider;
 import javax.microedition.location.QualifiedCoordinates;
 
 /**
- * @author user
+ * @author Mark Zaslavskiy
  *
  */
 public class LocationUtil {
@@ -17,25 +17,30 @@ public class LocationUtil {
 	
 	public static QualifiedCoordinates getCoordinates(){
 		
+		Location location = getLocation();
+		if (location == null)
+			return null;
+		
+		return location.getQualifiedCoordinates();
+        
+	}
+	
+	public static Location getLocation(){
 		if (!isLocationSupported()){
 			return null;
 		}
 		
-		Criteria cr = new Criteria();
-        cr.setHorizontalAccuracy(500);
-        LocationProvider lp;
+		Criteria criteria = new Criteria();
+        criteria.setHorizontalAccuracy(500);
+        LocationProvider locationProvider;
 		try {
-			lp = LocationProvider.getInstance(cr);
-			Location l = lp.getLocation(10);
-			QualifiedCoordinates qc = l.getQualifiedCoordinates();
-	        
-	        return qc;
+			locationProvider = LocationProvider.getInstance(criteria);
+			Location location = locationProvider.getLocation(10);	        
+	        return location;
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return null;
 		}
-        
 	}
 	
 	private static boolean isLocationSupported() {
