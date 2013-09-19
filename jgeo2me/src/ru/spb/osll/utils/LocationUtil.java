66055b9e9d9b023e38cff8f1ddf352135b6a274f
Mark Zaslavskiy <mark.zaslavskiy>
2013-09-19 21:46:54 +0400

    small fix

--- jgeo2me/src/ru/spb/osll/utils/LocationUtil.java
+++ jgeo2me/src/ru/spb/osll/utils/LocationUtil.java
@@ -5,8 +5,10 @@ package ru.spb.osll.utils;
 
 import javax.microedition.location.Criteria;
 import javax.microedition.location.Location;
+import javax.microedition.location.LocationException;
 import javax.microedition.location.LocationProvider;
 import javax.microedition.location.QualifiedCoordinates;
+
 
 /**
  * @author Mark Zaslavskiy
@@ -30,12 +32,11 @@ public class LocationUtil {
 			return null;
 		}
 		
-		Criteria criteria = new Criteria();
-		criteria.setHorizontalAccuracy(500);
-		LocationProvider locationProvider;
+
 		try {
-			locationProvider = LocationProvider.getInstance(criteria);
-			Location location = locationProvider.getLocation(10);	        
+			LocationProvider locationProvider = getLocationProvider();
+			
+			Location location = locationProvider.getLocation(60);	    
 	        return location;
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -43,13 +44,43 @@ public class LocationUtil {
 		}
 	}
 	
-	public static boolean isLocationSupported() {
-        boolean isItTrue = true;
+	private static LocationProvider getLocationProvider(){
+		LocationProvider locationProvider = null;
+
+			Criteria criteria = new Criteria();
+			//criteria.setHorizontalAccuracy(500);
+	        criteria.setPreferredPowerConsumption(Criteria.NO_REQUIREMENT);
+	        criteria.setSpeedAndCourseRequired(false);
+	        criteria.setAltitudeRequired(false);
+	        criteria.setAddressInfoRequired(false);
+	        
+	        try {
+				locationProvider = LocationProvider.getInstance(criteria);
+			} catch (LocationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+		return locationProvider;
+	}
+	
+	
+	
+	private static boolean isClassSupported(String className){
+		        boolean isItTrue = true;
             try {
-                Class.forName("javax.microedition.location.Location");
+                Class.forName(className);
             } catch (Exception e) {
                 isItTrue = false;
             }
         return isItTrue;
+	}
+	
+	public static boolean isLocationSupported() {
+		return isClassSupported("javax.microedition.location.Location");
     }
+	
+	public static boolean isLocationUtilSupported(){
+		return isClassSupported("com.nokia.mid.location.LocationUtil");
+	}
 }
